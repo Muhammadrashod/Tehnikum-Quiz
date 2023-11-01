916c400d5e4bde106abf0c5b42370c699218888c
Muhammadrashod Akromov <rashod0730@icloud.com>
2023-11-01 21:12:15 +0500

    commit

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,40 +1,33 @@
 import React from "react";
+import { Heading } from "../components/heading";
+import { Button } from "../components/Button";
+import { Input } from "../components/input";
 
 const Welcome = () => {
   return (
     <div className="container">
       <div className="wrapper">
         <div className="welcome">
-          <h1>Добро пожаловать в квиз от лучшего учебного центра</h1>
+          <Heading
+            text="Добро пожаловать в квиз от лучшего учебного центра"
+            headingType="h1"
+          />
           <form className="welcome__form">
-            <label className="input-wrapper" htmlFor="username">
-              Ваше имя
-              <input
-                required
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Ваш ответ"
-              />
-              <span id="error-message">
-                Введите номер в правильном формате например
-              </span>
-            </label>
-            <label className="input-wrapper" htmlFor="username">
-              Ваш номер
-              <input
-                required
-                type="tel"
-                name="phone"
-                id="phone"
-                placeholder="+998 9- --- -- -- "
-                pattern="^(?:\+998)?(?:\d{2})?(?:\d{7})$"
-              />
-              <span id="error-message">Введите номер в правильном формате</span>
-            </label>
-            <button disabled type="submit" id="next-btn">
-              Далее
-            </button>
+            <Input
+              id="username"
+              isRequired
+              inputLabel="Ваше имя"
+              inputPlaceholder="Ваш ответ"
+              errorMessage="Введите ваше имя"
+            />
+            <Input
+              id="phone"
+              isRequired
+              inputLabel="Ваш номер"
+              inputPlaceholder="+998 9- --- -- --"
+              errorMessage="Введите номер в правильном формате"
+            />
+            <Button buttonType="button" buttonText="Далее" />
           </form>
         </div>
       </div>
