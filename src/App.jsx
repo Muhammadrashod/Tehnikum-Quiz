310ab1efb6dd5c7ffba2782e4ea5e7000edac1f0
Muhammadrashod Akromov <rashod0730@icloud.com>
2023-11-06 18:39:59 +0500

    nf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import Welcome from "./pages/Welcome";
-import StepOne from './pages/StepOne';
+import Welcome from "./pages/Welcome";
+// import StepOne from './pages/StepOne';no
 import "./styles/main.css";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Welcome /> */}
-      <StepOne />
+      <Welcome />
+      {/* <StepOne /> */}
     </div>
   );
 };
